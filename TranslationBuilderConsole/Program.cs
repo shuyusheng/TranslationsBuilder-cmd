cc583d8d382add10e0cc52319711278fd38d009d
Yusheng Shu <yusheng.shu@fenwick.com.au>
2024-05-31 13:59:54 +1000

    Added console project

---
+++ TranslationBuilderConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using Mono.Options;
+
+namespace TranslationBuyilderConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://www.ndesk.org/doc/ndesk-options/NDesk.Options/OptionSet.html
+            bool show_help = false;
+            string inputfile = "";
+            string outputdir = "";
+            Version version = new Version();
+
+            var p = new OptionSet() {
+                { "h|help",  "show this message and exit", v => show_help = v != null },
+                { "i|inputfile=",  "input xml file (required)", v => inputfile = v },
+                { "o|outputdir=",  "output directory (if not specified, then input file name will be used)", v => outputdir = v },
+                { "v|version=",    "target Business Central version", v => version = new Version(v) }
+            };
+        }
+    }
+}
